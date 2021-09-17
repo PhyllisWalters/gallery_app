ae1537bef1c167d050da6d604af31cc22652b73e
EmmanuelFenyi <phylliswaltersandoh@email.com>
2021-09-17 16:54:42 +0000

    Final gallery app project

---
+++ lib/user_data.dart
@@ -0,0 +1,23 @@
+List userData = [
+  {
+    "userName": "Kwame Jantua",
+    "userLocation": "Adisadel Estate",
+    "userProfileImage": "images/profile_1.jpeg",
+    "userPost": "images/picture_1.jpeg",
+    "userPostLikes": "23",
+  },
+  {
+    "userName": "Kwame Jantua",
+    "userLocation": "Adisadel Estate",
+    "userProfileImage": "images/profile_2.jpeg",
+    "userPost": "images/picture_2.jpeg",
+    "userPostLikes": "23",
+  },
+  {
+    "userName": "Kwame Jantua",
+    "userLocation": "Adisadel Estate",
+    "userProfileImage": "images/pr0file_3.jpeg",
+    "userPost": "images/picture_3.jpeg",
+    "userPostLikes": "23",
+  }
+];
